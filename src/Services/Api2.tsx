915396233287a745dcc74ba867caa0a5809ae717
harshitShukla <harshit@AAIZELTECH.COM>
2025-12-21 23:31:06 +0530

    fix the clear code error

--- src/Services/Api2.tsx
+++ src/Services/Api2.tsx
@@ -224,7 +224,7 @@ const CommandAPIFrontend = () => {
                   <option value="scan">Scan</option>
                   <option value="live_data">Live Data</option>
                   <option value="vin">vin</option>
-                  <option value="clear_codes">Clear Codes</option>
+                  <option value="clear">Clear Codes</option>
                 </select>
               </div>
 
